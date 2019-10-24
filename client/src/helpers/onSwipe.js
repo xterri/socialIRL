8a6de43ca7a904adc2611fa5f307aa9791a7758f
terri <thuynh1989@gmail.com>
2019-10-23 21:21:09 -0600

    created file to write swipe methods and functions

---
+++ client/src/helpers/onSwipe.js
@@ -0,0 +1,16 @@
+
+export const EventLeftSwipe = (item) => { 
+    // archive/save event id to user's db
+
+    // remove/filter event from the state
+
+    // return new updated list (done in the context?)
+    console.log(item);
+};
+
+export const EventRightSwipe = (item) => { 
+    // remove/filter event from the state
+
+    // return new updated list (done in the context?)
+    console.log('RIGHT / DISLIKE SWIPE', item.title);
+};
