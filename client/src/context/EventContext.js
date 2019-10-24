c077a37df21557d162f70801c85da676562a97b3
terri <thuynh1989@gmail.com>
2019-10-23 21:20:17 -0600

    adding EventContext functions. Need to create db for it

--- client/src/context/EventContext.js
+++ client/src/context/EventContext.js
@@ -1,25 +1,35 @@
+import appAPI from '../api/appAPI';
 import createDataContext from './createDataContext'; 
-import jsonServer from '../api/jsonServer'; // how is this connected?
+
+// temp. until Events DB set up
+import data from '../devSource/events.json';
 
 const eventDetailsReducer = (state, action) => {
     switch (action.type) {
         case 'get_eventDetails':
-            return state;
+            return action.payload;
         default:
             return state;
     }
 };
 
-
 const getEventDetails = (dispatch) => {
     return (async () => {
-        const response = await jsonServer.get('/eventDetails'); 
-        dispatch({ type: 'get_eventDetails', payload: response.data }); 
+        // const response = await jsonServer.get('/eventDetails'); 
+
+        dispatch({ type: 'get_eventDetails', payload: data.eventDetails }); 
     });
 }
+
+const addEvent = (dispatch) => {
+    // TODO: List what contents need to be added to db
+    return (async (content) => {
+        await jsonServer.post('/eventDetails', { content }); 
+    });
+};
 
 // pass in reducer, object w/ actions, & initial/default state
 export const { Context, Provider } = createDataContext(
     eventDetailsReducer, 
-    { addEventDetails }, 
+    { addEvent, getEventDetails }, 
     []);
