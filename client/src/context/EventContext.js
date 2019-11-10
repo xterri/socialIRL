e38cf5c46f58815bc36b99604eed8e36db24c4a5
terri <thuynh1989@gmail.com>
2019-11-09 22:53:11 -0700

    adding addEvents action

--- client/src/context/EventContext.js
+++ client/src/context/EventContext.js
@@ -1,35 +1,41 @@
+import { AsyncStorage } from 'react-native';
+
 import appAPI from '../api/appAPI';
 import createDataContext from './createDataContext'; 
 
-// temp. until Events DB set up
-import data from '../devSource/events.json';
 
 const eventDetailsReducer = (state, action) => {
     switch (action.type) {
-        case 'get_eventDetails':
+        case 'get_events':
             return action.payload;
         default:
             return state;
     }
 };
 
-const getEventDetails = (dispatch) => {
+const getEvents = (dispatch) => {
     return (async () => {
-        // const response = await jsonServer.get('/eventDetails'); 
+        const response = await appAPI.get('/events'); 
 
-        dispatch({ type: 'get_eventDetails', payload: data.eventDetails }); 
+        dispatch({ type: 'get_events', payload: response.data }); 
     });
 }
 
 const addEvent = (dispatch) => {
     // TODO: List what contents need to be added to db
-    return (async (content) => {
-        await jsonServer.post('/eventDetails', { content }); 
+    return (async (title, description, eventDate, callback) => {
+        try {
+            await appAPI.post('/events', { title, description, eventDate }); 
+
+            callback ? callback() : null;
+        } catch (err) {
+            console.error(err);
+        }
     });
 };
 
 // pass in reducer, object w/ actions, & initial/default state
 export const { Context, Provider } = createDataContext(
     eventDetailsReducer, 
-    { addEvent, getEventDetails }, 
+    { addEvent, getEvents }, 
     []);
--- client/src/screens/CreateEventScreen.js
+++ client/src/screens/CreateEventScreen.js
@@ -1,14 +1,62 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
-const CreateEventScreen = () => {
+import { Context as EventContext } from '../context/EventContext';
+
+const CreateEventScreen = ({ navigation }) => {
+    const { addEvent } = useContext(EventContext);
+
+    const [title, setTitle] = useState('');
+    const [description, setContent] = useState('');
+    const [eventDate, setDate] = useState('');
+
     return (
         <View>
             <Text style={{ fontSize: 40 }}>CREATE EVENTS</Text>
+            
+            <View>
+                <Text style={styles.label}>Title</Text>
+                <TextInput 
+                    value={title}
+                    onChangeText={(newTitle) => setTitle(newTitle)}
+                    style={styles.input}
+                />
+                <Text style={styles.label}>Description</Text>
+                <TextInput 
+                    value={description}
+                    onChangeText={(newDescription) => setContent(newDescription)}
+                    style={styles.input}
+                />
+                <Text style={styles.label}>Event Date</Text>
+                <TextInput 
+                    value={eventDate}
+                    onChangeText={(newDate) => setDate(newDate)}
+                    style={styles.input}
+                />
+                <Button 
+                    title='Create Event' 
+                    onPress={() => addEvent(title, description, eventDate, () => {navigation.navigate('AccountMain')})}
+                />
+            </View>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    input: {
+        fontSize: 18,
+        backgroundColor: '#eee',
+        borderColor: '#ccc',
+        borderWidth: 1,
+        marginBottom: 15,
+        padding: 5,
+        margin: 5,
+    },
+    label: {
+        fontSize: 20, 
+        marginBottom: 5,
+        marginLeft: 5,
+    }
+});
 
 export default CreateEventScreen;
