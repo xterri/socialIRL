a73abc796a6a0b0965c475e6ecfe73938829eb19
terri <thuynh1989@gmail.com>
2019-11-09 22:51:48 -0700

    adding auto authentication whenever api is called/used

--- client/src/api/appAPI.js
+++ client/src/api/appAPI.js
@@ -1,6 +1,30 @@
+import { AsyncStorage } from 'react-native';
 import axios from 'axios';
 
-export default axios.create({
+const instance = axios.create({
     // enter public url for access to api; for now use ngrok (connect it to the server/expressAPI)
-    baseURL: 'https://socialirl.appspot.com/'
+    baseURL: 'https://88a07160.ngrok.io/' // dev
+    // baseURL: 'https://socialirl.appspot.com/' // prod 
 });
+
+// automatically authenticates the user to use the app; called every time we use our api
+instance.interceptors.request.use(
+    // called automatically when about to make a request
+    async (config) => {
+        const token = await AsyncStorage.getItem('token');
+
+        if (token) {
+            // adding authorization to header to use for authentication
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        // return modified config object
+        return config;
+    },
+    // called automatically when there is an error with request
+    (err) => {
+        return Promise.reject(err);
+    }
+);
+
+export default instance;
